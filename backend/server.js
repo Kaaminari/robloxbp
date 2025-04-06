ec1ba1e5496bbe25bb332db280d9c35e160d1cb5
Kaaminari <rraazzer@gmail.com>
2025-04-06 09:31:57 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.static("public"));
 app.use(express.json());
 
 app.post("/refresh", (req, res) => {
