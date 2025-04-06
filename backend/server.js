f70ff49449b57b2cc1b5057570066a79ecd50189
Kaaminari <rraazzer@gmail.com>
2025-04-06 10:14:14 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,17 +18,17 @@ app.post("/refresh", async (req, res) => {
   }
 
   try {
-    const response = await fetch("https://auth.roblox.com/v2/logout", {
-      method: "POST",
-      headers: {
-        "Cookie": `.ROBLOSECURITY=${cookie}`,
-        "User-Agent": "Mozilla/5.0",
-        "Accept": "application/json",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Connection": "keep-alive",
-        "Content-Type": "application/json",
-      },
-    });
+const response = await fetch("https://users.roblox.com/v1/users/authenticated", {
+  method: "GET",
+  headers: {
+    "Cookie": `.ROBLOSECURITY=${cookie}`,
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "application/json",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Connection": "keep-alive",
+  },
+});
+
 
     // Logando o status da resposta e os headers
     console.log("Status da resposta:", response.status);
