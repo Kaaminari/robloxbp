7520af1db60befde407f1fcb6287f43708fdeedf
Kaaminari <rraazzer@gmail.com>
2025-04-08 08:40:00 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { alterarIdade } = require('./roblox');
 
 const app = express();
-const port = process.env.PORT || 3005;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
