2079925112f57760cee8ceb8b9ebf41c81b897cf
Kaaminari <rraazzer@gmail.com>
2025-04-11 11:55:53 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { alterarIdade } = require('./roblox');
+require('dotenv').config(); // carrega variáveis do .env
 
 const app = express();
 const port = process.env.PORT || 3001;
